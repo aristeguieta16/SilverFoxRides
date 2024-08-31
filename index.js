735cf2c35acb2766b8360469a0a105534a6f3ad8
Miguelangel Aristeguieta <ma19bf@fsu.edu>
2024-08-30 20:07:39 -0400

    Updated CORS and payment-confirmation endpoint setup

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
+const cors = require('cors');
+
+app.use(cors());
 
 // Initialize Express
 const app = express();
